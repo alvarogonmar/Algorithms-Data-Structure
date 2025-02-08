6b21c3275a2479383929fdd4ba8929a49aa08b3c
alvarogonmar <alvarogonmar0@gmail.com>
2025-02-08 12:49:10 -0600

    Add inline comments to add method in MyQueue class for improved clarity

--- algoritmos-estructuras-datos-entrevistas-udemy-main/05-StacksQueues/src/main/java/com/danielblanco/algoritmosestructuras/_00_stackqueue/MyQueue.java
+++ algoritmos-estructuras-datos-entrevistas-udemy-main/05-StacksQueues/src/main/java/com/danielblanco/algoritmosestructuras/_00_stackqueue/MyQueue.java
@@ -4,10 +4,10 @@ public class MyQueue {
   private Node first; // Almacenar el primer nodo de la cola
   private Node last; // Almacenar el último nodo de la cola
 
-  public void add(int value) {
-    Node newLast = new Node(value);
-    if (last != null) {
-      last.next = newLast;
+  public void add(int value) { // Agregar un nodo al final de la cola
+    Node newLast = new Node(value); // Crear un nuevo nodo con el valor
+    if (last != null) { // Si la cola no está vacía
+      last.next = newLast; // El siguiente nodo del último nodo es el nuevo nodo
     }
 
     last = newLast;
