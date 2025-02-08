82e5ab64b36c28797c01daea8cc296372a5aa96a
alvarogonmar <alvarogonmar0@gmail.com>
2025-02-08 12:45:03 -0600

    Refactor MyEmptyQueueException and MyStack classes to improve code clarity with added comments

--- algoritmos-estructuras-datos-entrevistas-udemy-main/05-StacksQueues/src/main/java/com/danielblanco/algoritmosestructuras/_00_stackqueue/MyEmptyQueueException.java
+++ algoritmos-estructuras-datos-entrevistas-udemy-main/05-StacksQueues/src/main/java/com/danielblanco/algoritmosestructuras/_00_stackqueue/MyEmptyQueueException.java
@@ -1,3 +1,4 @@
 package com.danielblanco.algoritmosestructuras._00_stackqueue;
 
-public class MyEmptyQueueException extends RuntimeException {}
+public class MyEmptyQueueException extends RuntimeException {
+}
--- algoritmos-estructuras-datos-entrevistas-udemy-main/05-StacksQueues/src/main/java/com/danielblanco/algoritmosestructuras/_00_stackqueue/MyStack.java
+++ algoritmos-estructuras-datos-entrevistas-udemy-main/05-StacksQueues/src/main/java/com/danielblanco/algoritmosestructuras/_00_stackqueue/MyStack.java
@@ -1,12 +1,12 @@
 package com.danielblanco.algoritmosestructuras._00_stackqueue;
 
 public class MyStack {
-  private Node top;
+  private Node top; // Almacenar el nodo superior (top) de la pila
 
-  public void push(int value) {
-    Node newTop = new Node(value);
-    newTop.next = top;
-    top = newTop;
+  public void push(int value) { //
+    Node newTop = new Node(value); // Crear un nuevo nodo con el valor
+    newTop.next = top; // El siguiente nodo del nuevo nodo es el nodo superior actual
+    top = newTop; // El nuevo nodo es el nodo superior
   }
 
   public int pop() {
