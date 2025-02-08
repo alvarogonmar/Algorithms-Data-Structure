4d548307ea29ad10bcd531564385519f5d44c159
alvarogonmar <alvarogonmar0@gmail.com>
2025-02-07 20:41:25 -0600

    Refactor AddTwoNumbers method to initialize result node and remove redundant logic

--- algoritmos-estructuras-datos-entrevistas-udemy-main/04-LinkedLists/src/main/java/com/danielblanco/algoritmosestructuras/_04_add_two_numbers/AddTwoNumbers.java
+++ algoritmos-estructuras-datos-entrevistas-udemy-main/04-LinkedLists/src/main/java/com/danielblanco/algoritmosestructuras/_04_add_two_numbers/AddTwoNumbers.java
@@ -19,30 +19,6 @@ public class AddTwoNumbers {
     int carry = 0;
 
     while (list1 != null || list2 != null) {
-      current.next = new Node(-1);
-      current = current.next;
-      int digit = carry;
-
-      if (list1 != null) {
-        digit += list1.value;
-        list1 = list1.next;
-      }
-
-      if (list2 != null) {
-        digit += list2.value;
-        list2 = list2.next;
-      }
-
-      carry = digit / 10;
-      digit = digit % 10;
-
-      current.value = digit;
-    }
-
-    if (carry > 0) {
-      current.next = new Node(carry);
-    }
-
-    return result.next;
+      Node result = new Node(-1); // Crear un nuevo nodo
   }
 }
