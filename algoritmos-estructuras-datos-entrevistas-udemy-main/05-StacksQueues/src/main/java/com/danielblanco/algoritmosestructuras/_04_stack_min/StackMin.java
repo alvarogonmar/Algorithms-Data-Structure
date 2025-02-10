dcb0e39e12357045f76665ad82f3f7a2eb9191c7
alvarogonmar <alvarogonmar0@gmail.com>
2025-02-09 20:42:51 -0600

    Add valuesStack to StackMin class for storing stack values

--- algoritmos-estructuras-datos-entrevistas-udemy-main/05-StacksQueues/src/main/java/com/danielblanco/algoritmosestructuras/_04_stack_min/StackMin.java
+++ algoritmos-estructuras-datos-entrevistas-udemy-main/05-StacksQueues/src/main/java/com/danielblanco/algoritmosestructuras/_04_stack_min/StackMin.java
@@ -1,10 +1,15 @@
 package com.danielblanco.algoritmosestructuras._04_stack_min;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 /*
  * ¿Cómo diseñarías un Stack que además de las operaciones de push y pop también
  * contase con una operación para obtener el mínimo?
  */
 public class StackMin {
+
+  private Deque<Integer> valuesStack = new ArrayDeque<>(); // Stack para los valores
 
   public void push(Integer data) {
     throw new UnsupportedOperationException("Not implemented yet");
