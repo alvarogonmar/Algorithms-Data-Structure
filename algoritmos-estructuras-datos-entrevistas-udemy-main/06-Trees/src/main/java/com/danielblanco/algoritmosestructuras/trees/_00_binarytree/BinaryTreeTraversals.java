6bedff7a88fd63aab6333d439e2d85444d7a5961
alvarogonmar <alvarogonmar0@gmail.com>
2025-02-10 18:45:37 -0600

    Add comments to inOrderTraversal method for better code clarity

--- algoritmos-estructuras-datos-entrevistas-udemy-main/06-Trees/src/main/java/com/danielblanco/algoritmosestructuras/trees/_00_binarytree/BinaryTreeTraversals.java
+++ algoritmos-estructuras-datos-entrevistas-udemy-main/06-Trees/src/main/java/com/danielblanco/algoritmosestructuras/trees/_00_binarytree/BinaryTreeTraversals.java
@@ -5,9 +5,9 @@ package com.danielblanco.algoritmosestructuras.trees._00_binarytree;
 public class BinaryTreeTraversals { // Clase para los recorridos de un árbol binario
   public static void inOrderTraversal(Node node) { // Recorrido inOrder
     if (node != null) { // Si el nodo no es nulo
-      inOrderTraversal(node.left);
-      System.out.print(node.value + " ");
-      inOrderTraversal(node.right);
+      inOrderTraversal(node.left); // Recorre el nodo izquierdo
+      System.out.print(node.value + " "); // Imprime el valor del nodo
+      inOrderTraversal(node.right); // Recorre el nodo derecho
     }
   }
 
