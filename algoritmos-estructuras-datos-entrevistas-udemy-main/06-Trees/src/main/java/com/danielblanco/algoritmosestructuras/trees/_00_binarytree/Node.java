66ef842c5d7d364cdf70c236064396d2f8303c67
alvarogonmar <alvarogonmar0@gmail.com>
2025-02-10 18:43:40 -0600

    Add comments to Node class for better code clarity

--- algoritmos-estructuras-datos-entrevistas-udemy-main/06-Trees/src/main/java/com/danielblanco/algoritmosestructuras/trees/_00_binarytree/Node.java
+++ algoritmos-estructuras-datos-entrevistas-udemy-main/06-Trees/src/main/java/com/danielblanco/algoritmosestructuras/trees/_00_binarytree/Node.java
@@ -1,9 +1,9 @@
 package com.danielblanco.algoritmosestructuras.trees._00_binarytree;
 
-public class Node {
-  public int value;
-  public Node left;
-  public Node right;
+public class Node { // Clase para los nodos del árbol binario
+  public int value; // Valor del nodo
+  public Node left; // Nodo izquierdo
+  public Node right; // Nodo derecho
 
   public Node(int value) {
     this.value = value;
