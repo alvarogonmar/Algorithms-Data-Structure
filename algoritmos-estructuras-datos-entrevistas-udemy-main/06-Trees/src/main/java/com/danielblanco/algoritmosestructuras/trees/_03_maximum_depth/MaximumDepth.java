b34aac0be8688eedcffc5bd6e1cce43654965f58
alvarogonmar <alvarogonmar0@gmail.com>
2025-02-11 18:51:28 -0600

    Add calculation for left subtree depth in maxDepth method

--- algoritmos-estructuras-datos-entrevistas-udemy-main/06-Trees/src/main/java/com/danielblanco/algoritmosestructuras/trees/_03_maximum_depth/MaximumDepth.java
+++ algoritmos-estructuras-datos-entrevistas-udemy-main/06-Trees/src/main/java/com/danielblanco/algoritmosestructuras/trees/_03_maximum_depth/MaximumDepth.java
@@ -19,6 +19,9 @@ public class MaximumDepth {
   public int maxDepth(Node root) {
     if (root == null) { // Si el nodo raíz es nulo, devolvemos 0
       return 0; // Devolvemos 0
+    } else {
+      int leftDepth = maxDepth(root.left) + 1; // Calculamos la profundidad del nodo izquierdo y le sumamos 1 (por el
+                                               // nodo raíz)
     }
   }
 }
