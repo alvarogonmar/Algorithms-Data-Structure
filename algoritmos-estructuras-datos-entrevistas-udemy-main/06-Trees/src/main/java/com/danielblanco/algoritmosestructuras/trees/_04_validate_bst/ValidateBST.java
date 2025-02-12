a6923b74b077210bc1512e2a78a8dd1abb63c063
alvarogonmar <alvarogonmar0@gmail.com>
2025-02-11 19:25:26 -0600

    Call isValidBST with min and max parameters in the main isValidBST method

--- algoritmos-estructuras-datos-entrevistas-udemy-main/06-Trees/src/main/java/com/danielblanco/algoritmosestructuras/trees/_04_validate_bst/ValidateBST.java
+++ algoritmos-estructuras-datos-entrevistas-udemy-main/06-Trees/src/main/java/com/danielblanco/algoritmosestructuras/trees/_04_validate_bst/ValidateBST.java
@@ -28,6 +28,7 @@ import com.danielblanco.algoritmosestructuras.trees._00_binarytree.Node;
 public class ValidateBST {
 
   public boolean isValidBST(Node root) {
+    return isValidBST(root, null, null); // llamamos a la funcion con el maximo y minimo
   }
 
   public boolean isValidBST(Node root, Integer min, Integer max) { // funcion con el maximo y minimo
