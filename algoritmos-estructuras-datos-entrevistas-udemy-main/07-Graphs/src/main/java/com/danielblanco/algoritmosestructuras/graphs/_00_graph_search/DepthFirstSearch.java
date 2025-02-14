43afdb3d85f17857c96ecf151194589c8f439353
alvarogonmar <alvarogonmar0@gmail.com>
2025-02-13 19:36:06 -0600

    Add comments to clarify the recursive call and return value in depthFirstSearch method

--- algoritmos-estructuras-datos-entrevistas-udemy-main/07-Graphs/src/main/java/com/danielblanco/algoritmosestructuras/graphs/_00_graph_search/DepthFirstSearch.java
+++ algoritmos-estructuras-datos-entrevistas-udemy-main/07-Graphs/src/main/java/com/danielblanco/algoritmosestructuras/graphs/_00_graph_search/DepthFirstSearch.java
@@ -3,8 +3,8 @@ package com.danielblanco.algoritmosestructuras.graphs._00_graph_search;
 public class DepthFirstSearch {
   public static boolean depthFirstSearch(Graph graph, String target) { // Funcion que recibe un grafo y un objetivo
     for (GraphNode node : graph.nodes.values()) { // iterar sobre los nodos del grafo
-      if (recursiveDfsHelper(node, target)) {
-        return true;
+      if (recursiveDfsHelper(node, target)) { // Llamar a la función auxiliar recursiva
+        return true; // Si la función auxiliar devuelve verdadero, devolver verdadero
       }
     }
 
