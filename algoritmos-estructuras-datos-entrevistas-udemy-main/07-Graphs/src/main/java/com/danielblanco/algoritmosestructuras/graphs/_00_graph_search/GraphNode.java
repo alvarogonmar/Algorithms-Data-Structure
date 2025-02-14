061d63c361a17e6e2075053ddda03b88da6c8b7e
alvarogonmar <alvarogonmar0@gmail.com>
2025-02-13 19:23:12 -0600

    Add comment to clarify the use of HashMap for storing adjacent nodes in GraphNode

--- algoritmos-estructuras-datos-entrevistas-udemy-main/07-Graphs/src/main/java/com/danielblanco/algoritmosestructuras/graphs/_00_graph_search/GraphNode.java
+++ algoritmos-estructuras-datos-entrevistas-udemy-main/07-Graphs/src/main/java/com/danielblanco/algoritmosestructuras/graphs/_00_graph_search/GraphNode.java
@@ -6,6 +6,8 @@ public class GraphNode { // Clase para representar un nodo de un grafo
   public String value; // Almacena el valor del nodo
   public GraphNodeStatus status; // Almacena el estado del nodo (visitado, no visitado, visitando)
   public HashMap<String, GraphNode> adjacents; // Almacena los nodos adyacentes al nodo actual
+  // Utilizas hashMap para almacenar los nodos adyacentes, ya que es más
+  // eficiente, con clave el valor del nodo y valor el nodo
 
   public GraphNode(String value) {
     this.value = value;
