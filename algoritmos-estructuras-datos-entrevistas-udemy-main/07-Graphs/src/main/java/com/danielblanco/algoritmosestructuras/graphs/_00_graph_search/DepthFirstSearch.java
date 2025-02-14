ed5da21e3ad8dcb09e6d660f437518ff1f3c53f6
alvarogonmar <alvarogonmar0@gmail.com>
2025-02-13 19:35:21 -0600

    Add comments to clarify the depthFirstSearch method in DepthFirstSearch class

--- algoritmos-estructuras-datos-entrevistas-udemy-main/07-Graphs/src/main/java/com/danielblanco/algoritmosestructuras/graphs/_00_graph_search/DepthFirstSearch.java
+++ algoritmos-estructuras-datos-entrevistas-udemy-main/07-Graphs/src/main/java/com/danielblanco/algoritmosestructuras/graphs/_00_graph_search/DepthFirstSearch.java
@@ -1,7 +1,7 @@
 package com.danielblanco.algoritmosestructuras.graphs._00_graph_search;
 
 public class DepthFirstSearch {
-  public static boolean depthFirstSearch(Graph graph, String target) {
+  public static boolean depthFirstSearch(Graph graph, String target) { // Funcion que recibe un grafo y un objetivo
     for (GraphNode node : graph.nodes.values()) {
       if (recursiveDfsHelper(node, target)) {
         return true;
@@ -12,13 +12,15 @@ public class DepthFirstSearch {
   }
 
   private static boolean recursiveDfsHelper(GraphNode currentNode, String target) {
-    if (currentNode.value.equals(target)) return true;
+    if (currentNode.value.equals(target))
+      return true;
 
     currentNode.status = GraphNodeStatus.Visited;
 
     for (GraphNode node : currentNode.adjacents.values()) {
       if (node.status != GraphNodeStatus.Visited) {
-        if (recursiveDfsHelper(node, target)) return true;
+        if (recursiveDfsHelper(node, target))
+          return true;
       }
     }
     return false;
