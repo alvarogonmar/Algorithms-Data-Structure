c72675ab186346a0459ef031d457a3d4cbc53f46
alvarogonmar <alvarogonmar0@gmail.com>
2025-02-16 11:41:42 -0600

    Add check for existing node in map before cloning in CloneGraph class

--- algoritmos-estructuras-datos-entrevistas-udemy-main/07-Graphs/src/main/java/com/danielblanco/algoritmosestructuras/graphs/_02_clone_graph/CloneGraph.java
+++ algoritmos-estructuras-datos-entrevistas-udemy-main/07-Graphs/src/main/java/com/danielblanco/algoritmosestructuras/graphs/_02_clone_graph/CloneGraph.java
@@ -25,5 +25,7 @@ public class CloneGraph {
   private Node clone(Node node, HashMap<Integer, Node> map) { // funcion para clonar un nodo
     if (node == null)
       return null; // si el nodo es null, return null
+    
+    if (map.containsKey(node.val)) // si el map contiene la key node.val
   }
 }
