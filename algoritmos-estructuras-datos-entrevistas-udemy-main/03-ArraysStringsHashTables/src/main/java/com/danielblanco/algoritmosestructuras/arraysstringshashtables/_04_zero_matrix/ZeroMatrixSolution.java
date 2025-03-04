48002e2abea94a0b9ce1e8c43c1d9c364058a868
alvarogonmar <alvarogonmar0@gmail.com>
2025-03-04 06:48:48 -0600

    Add comment to clarify the purpose of hasFirstRowAnyZeroes method in ZeroMatrixSolution class

--- algoritmos-estructuras-datos-entrevistas-udemy-main/03-ArraysStringsHashTables/src/main/java/com/danielblanco/algoritmosestructuras/arraysstringshashtables/_04_zero_matrix/ZeroMatrixSolution.java
+++ algoritmos-estructuras-datos-entrevistas-udemy-main/03-ArraysStringsHashTables/src/main/java/com/danielblanco/algoritmosestructuras/arraysstringshashtables/_04_zero_matrix/ZeroMatrixSolution.java
@@ -37,7 +37,7 @@ public class ZeroMatrixSolution { // O(n*m)
     }
   }
 
-  private boolean hasFirstRowAnyZeroes(int[][] matrix) {
+  private boolean hasFirstRowAnyZeroes(int[][] matrix) { // funcion para saber si la primera fila tiene un 0
     for (int i = 0; i < matrix[0].length; i++) {
       if (matrix[0][i] == 0) {
         return true;
