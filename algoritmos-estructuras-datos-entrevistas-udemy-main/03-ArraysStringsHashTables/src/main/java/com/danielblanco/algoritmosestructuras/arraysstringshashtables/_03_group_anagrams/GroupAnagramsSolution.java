a358fd036d017ebbabbb8e07db8cd8fe2d42a680
alvarogonmar <alvarogonmar0@gmail.com>
2025-03-03 20:56:58 -0600

    Add comment to clarify the increment operation in getAnagramHash method

--- algoritmos-estructuras-datos-entrevistas-udemy-main/03-ArraysStringsHashTables/src/main/java/com/danielblanco/algoritmosestructuras/arraysstringshashtables/_03_group_anagrams/GroupAnagramsSolution.java
+++ algoritmos-estructuras-datos-entrevistas-udemy-main/03-ArraysStringsHashTables/src/main/java/com/danielblanco/algoritmosestructuras/arraysstringshashtables/_03_group_anagrams/GroupAnagramsSolution.java
@@ -42,7 +42,7 @@ public class GroupAnagramsSolution {
   private String getAnagramHash(String s) { // funcion para obtener el hash de un string
     int[] letterCount = new int[26]; // array para contar las letras de la palabra
     for (int c : s.toCharArray()) { // for para recorrer la palabra
-      letterCount[c - 'a']++;
+      letterCount[c - 'a']++; // incrementamos el contador de la letra
     }
 
     return Arrays.toString(letterCount);
