9f60e783f0402800bfbf63819868a05f31e01269
alvarogonmar <alvarogonmar0@gmail.com>
2025-03-12 23:04:15 -0600

    Add comment to clarify the action of setting a row to zero in setRowToZero method

--- algoritmos-estructuras-datos-entrevistas-udemy-main/03-ArraysStringsHashTables/src/main/java/com/danielblanco/algoritmosestructuras/arraysstringshashtables/_04_zero_matrix/ZeroMatrixSolution.java
+++ algoritmos-estructuras-datos-entrevistas-udemy-main/03-ArraysStringsHashTables/src/main/java/com/danielblanco/algoritmosestructuras/arraysstringshashtables/_04_zero_matrix/ZeroMatrixSolution.java
@@ -82,7 +82,7 @@ public class ZeroMatrixSolution { // O(n*m)
 
   private void setRowToZero(int[][] matrix, int row) { // funcion para establecer la fila a 0
     for (int col = 0; col < matrix[0].length; col++) { // recorremos las columnas
-      matrix[row][col] = 0;
+      matrix[row][col] = 0; // establecemos la fila a 0
     }
   }
 
