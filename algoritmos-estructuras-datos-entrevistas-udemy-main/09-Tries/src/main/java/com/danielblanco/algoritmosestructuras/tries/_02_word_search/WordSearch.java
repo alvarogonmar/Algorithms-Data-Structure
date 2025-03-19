04db80baa41ea0678d652197c52ca2ba08cc956c
alvarogonmar <alvarogonmar0@gmail.com>
2025-03-19 17:52:52 -0600

    Add null check for current TrieNode in dfs method of WordSearch class

--- algoritmos-estructuras-datos-entrevistas-udemy-main/09-Tries/src/main/java/com/danielblanco/algoritmosestructuras/tries/_02_word_search/WordSearch.java
+++ algoritmos-estructuras-datos-entrevistas-udemy-main/09-Tries/src/main/java/com/danielblanco/algoritmosestructuras/tries/_02_word_search/WordSearch.java
@@ -46,7 +46,9 @@ public class WordSearch {
   }
 
   private void dfs(char[][] board, TrieNode current, int row, int col, List<String> result) {
-
+    if (current == null) {
+      return;
+    }
   }
 
   private TrieNode buildTrie(String[] words, int maxLen) {
