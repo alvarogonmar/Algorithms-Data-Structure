9813920a506ffb40a85fcdcc0b436a00ba790d38
alvarogonmar <alvarogonmar0@gmail.com>
2025-03-24 21:49:41 -0600

    Add comment to clarify the creation of a new TrieNode during character insertion

--- algoritmos-estructuras-datos-entrevistas-udemy-main/09-Tries/src/main/java/com/danielblanco/algoritmosestructuras/tries/_01_title_suggestions/Trie.java
+++ algoritmos-estructuras-datos-entrevistas-udemy-main/09-Tries/src/main/java/com/danielblanco/algoritmosestructuras/tries/_01_title_suggestions/Trie.java
@@ -19,7 +19,7 @@ public class Trie {
     for (char c : word.toCharArray()) { // iterar sobre cada caracter de la palabra
       TrieNode child = currentNode.children.get(c); // Verificar si el caracter ya existe en el nodo actual
       if (child == null) { // Si no existe, crear un nuevo nodo
-        child = new TrieNode();
+        child = new TrieNode(); // Crear un nuevo nodo
         currentNode.children.put(c, child);
       }
       currentNode = child;
