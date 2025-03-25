6710ce3756aa860020fcaf430f030eee1aacec63
alvarogonmar <alvarogonmar0@gmail.com>
2025-03-24 21:51:00 -0600

    Add comment to clarify marking the current node as the end of a word

--- algoritmos-estructuras-datos-entrevistas-udemy-main/09-Tries/src/main/java/com/danielblanco/algoritmosestructuras/tries/_01_title_suggestions/Trie.java
+++ algoritmos-estructuras-datos-entrevistas-udemy-main/09-Tries/src/main/java/com/danielblanco/algoritmosestructuras/tries/_01_title_suggestions/Trie.java
@@ -25,7 +25,7 @@ public class Trie {
       currentNode = child; // Mover el puntero al nuevo nodo
     }
 
-    currentNode.isEndOfWord = true;
+    currentNode.isEndOfWord = true; // Marcar el nodo actual como fin de palabra
   }
 
   public boolean search(String prefix) {
