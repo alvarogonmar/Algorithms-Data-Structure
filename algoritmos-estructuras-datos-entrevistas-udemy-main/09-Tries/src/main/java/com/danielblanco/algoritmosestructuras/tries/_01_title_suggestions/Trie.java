ee26a2e7f7033806f1b4eb6b8b6d7236fe35cc6c
alvarogonmar <alvarogonmar0@gmail.com>
2025-03-25 20:51:55 -0600

    Add comment to clarify null node check in the search method

--- algoritmos-estructuras-datos-entrevistas-udemy-main/09-Tries/src/main/java/com/danielblanco/algoritmosestructuras/tries/_01_title_suggestions/Trie.java
+++ algoritmos-estructuras-datos-entrevistas-udemy-main/09-Tries/src/main/java/com/danielblanco/algoritmosestructuras/tries/_01_title_suggestions/Trie.java
@@ -37,7 +37,7 @@ public class Trie {
 
     for (char c : prefix.toCharArray()) { // Iterar sobre cada caracter del prefijo
       currentNode = currentNode.children.get(c); // Mover el puntero al siguiente nodo
-      if (currentNode == null)
+      if (currentNode == null) // Si el nodo es nulo, la palabra no existe
         return false;
     }
 
