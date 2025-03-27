6e59754a2a356cb35e2bfbdd64f9ab1bd4b6ea3e
alvarogonmar <alvarogonmar0@gmail.com>
2025-03-27 07:14:38 -0600

    Add comment to clarify search direction in DFS method for right movement in WordSearch class

--- algoritmos-estructuras-datos-entrevistas-udemy-main/09-Tries/src/main/java/com/danielblanco/algoritmosestructuras/tries/_02_word_search/WordSearch.java
+++ algoritmos-estructuras-datos-entrevistas-udemy-main/09-Tries/src/main/java/com/danielblanco/algoritmosestructuras/tries/_02_word_search/WordSearch.java
@@ -73,7 +73,7 @@ public class WordSearch {
     board[row][col] = '#'; // marca la posición como visitada
     TrieNode next = current.map.get(currentChar); // obtiene el siguiente nodo
     dfs(board, next, row - 1, col, result); // busca arriba
-    dfs(board, next, row, col + 1, result);
+    dfs(board, next, row, col + 1, result); // busca derecha
     dfs(board, next, row + 1, col, result);
     dfs(board, next, row, col - 1, result);
 
