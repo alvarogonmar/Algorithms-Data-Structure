fdab647cb2effa53c9ea5bf7b9b74963e81b97aa
alvarogonmar <alvarogonmar0@gmail.com>
2025-03-30 14:03:48 -0600

    Add comment to clarify return statement in dfs method of WordSearchSolution class

--- algoritmos-estructuras-datos-entrevistas-udemy-main/09-Tries/src/main/java/com/danielblanco/algoritmosestructuras/tries/_02_word_search/WordSearchSolution.java
+++ algoritmos-estructuras-datos-entrevistas-udemy-main/09-Tries/src/main/java/com/danielblanco/algoritmosestructuras/tries/_02_word_search/WordSearchSolution.java
@@ -53,7 +53,7 @@ public class WordSearchSolution {
 
   private void dfs(char[][] board, TrieNode current, int row, int col, List<String> result) {
     if (current == null) { // si el nodo es nulo
-      return;
+      return; // retorna
     }
 
     if (current.word != null) {
