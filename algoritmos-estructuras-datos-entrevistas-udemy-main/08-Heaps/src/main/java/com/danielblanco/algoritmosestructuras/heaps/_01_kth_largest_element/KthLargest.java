591b4ef39a4a93fb0a8a1c87ec680d5ef5724fa8
alvarogonmar <alvarogonmar0@gmail.com>
2025-04-06 13:48:18 -0600

    Add comment to clarify the purpose of adding elements to the heap in KthLargest class

--- algoritmos-estructuras-datos-entrevistas-udemy-main/08-Heaps/src/main/java/com/danielblanco/algoritmosestructuras/heaps/_01_kth_largest_element/KthLargest.java
+++ algoritmos-estructuras-datos-entrevistas-udemy-main/08-Heaps/src/main/java/com/danielblanco/algoritmosestructuras/heaps/_01_kth_largest_element/KthLargest.java
@@ -27,7 +27,7 @@ public class KthLargest {
     this.k = k; // kth elemento más grande
     heap = new PriorityQueue<>(); // MinHeap
 
-    for (int num : nums) {
+    for (int num : nums) { // Añade los elementos al heap
       heap.add(num);
     }
 
