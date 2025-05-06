88c211d3a02b0012cb47a5a2d0e6046fe4154b14
alvarogonmar <alvarogonmar0@gmail.com>
2025-05-05 18:03:40 -0600

    Add comment to clarify return value in maxDepth method

--- algoritmos-estructuras-datos-entrevistas-udemy-main/06-Trees/src/main/java/com/danielblanco/algoritmosestructuras/trees/_03_maximum_depth/MaximumDepthSolution.java
+++ algoritmos-estructuras-datos-entrevistas-udemy-main/06-Trees/src/main/java/com/danielblanco/algoritmosestructuras/trees/_03_maximum_depth/MaximumDepthSolution.java
@@ -17,7 +17,7 @@ import com.danielblanco.algoritmosestructuras.trees._00_binarytree.Node;
 public class MaximumDepthSolution { // Clase principal que calcula la profundidad máxima de un árbol binario
   public int maxDepth(Node root) { // Función principal que calcula la profundidad máxima
     if (root == null) { // Si el árbol está vacío
-      return 0;
+      return 0; // Devolver 0
 
     int depthLeft = maxDepth(root.left) + 1;
     int depthRight = maxDepth(root.right) + 1;
