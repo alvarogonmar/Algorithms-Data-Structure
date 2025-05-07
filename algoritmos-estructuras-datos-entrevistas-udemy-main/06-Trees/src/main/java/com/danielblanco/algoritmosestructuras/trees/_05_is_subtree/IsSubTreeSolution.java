2fcd98f608db4f1e6295967108176e4099debec6
alvarogonmar <alvarogonmar0@gmail.com>
2025-05-06 22:01:11 -0600

    Add comment to clarify the purpose of isSubtree method in IsSubTreeSolution class

--- algoritmos-estructuras-datos-entrevistas-udemy-main/06-Trees/src/main/java/com/danielblanco/algoritmosestructuras/trees/_05_is_subtree/IsSubTreeSolution.java
+++ algoritmos-estructuras-datos-entrevistas-udemy-main/06-Trees/src/main/java/com/danielblanco/algoritmosestructuras/trees/_05_is_subtree/IsSubTreeSolution.java
@@ -37,7 +37,7 @@ import com.danielblanco.algoritmosestructuras.trees._00_binarytree.Node;
  */
 public class IsSubTreeSolution { // Clase principal que determina si un árbol es subárbol de otro
 
-  public boolean isSubtree(Node first, Node second) {
+  public boolean isSubtree(Node first, Node second) { // Función principal que determina si un árbol es subárbol de otro
     StringBuilder stb1 = new StringBuilder();
     StringBuilder stb2 = new StringBuilder();
 
