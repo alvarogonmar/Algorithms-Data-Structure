67777430c78f96c673a1503372816e09462fa308
alvarogonmar <alvarogonmar0@gmail.com>
2025-05-16 07:20:41 -0600

    Add comment to clarify the insert method in the Trie class

--- algoritmos-estructuras-datos-entrevistas-udemy-main/09-Tries/src/main/java/com/danielblanco/algoritmosestructuras/tries/_01_title_suggestions/Trie.java
+++ algoritmos-estructuras-datos-entrevistas-udemy-main/09-Tries/src/main/java/com/danielblanco/algoritmosestructuras/tries/_01_title_suggestions/Trie.java
@@ -9,7 +9,7 @@ public class Trie { // Clase Trie para almacenar palabras y buscar prefijos
     this.ignoreCase = ignoreCase; // Asignar el valor de ignoreCase
   }
 
-  public void insert(String word) {
+  public void insert(String word) { // Insertar una palabra en el trie
     TrieNode currentNode = root;
 
     if (ignoreCase) {
