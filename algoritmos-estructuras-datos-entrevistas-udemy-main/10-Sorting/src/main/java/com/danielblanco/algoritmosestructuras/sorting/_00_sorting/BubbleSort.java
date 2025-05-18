63a87fc2ce8091902261cad3fa3bb9ba2982eb56
alvarogonmar <alvarogonmar0@gmail.com>
2025-05-17 22:39:41 -0600

    Add comment to clarify the condition in the inner loop of the sort method in BubbleSort class

--- algoritmos-estructuras-datos-entrevistas-udemy-main/10-Sorting/src/main/java/com/danielblanco/algoritmosestructuras/sorting/_00_sorting/BubbleSort.java
+++ algoritmos-estructuras-datos-entrevistas-udemy-main/10-Sorting/src/main/java/com/danielblanco/algoritmosestructuras/sorting/_00_sorting/BubbleSort.java
@@ -4,7 +4,7 @@ public class BubbleSort { // Bubble Sort
   static void sort(int[] array) { // método para ordenar el array
     for (int i = 0; i < array.length - 1; i++) { // recorre el array
       for (int j = 0; j < array.length - i - 1; j++) { // recorre el array
-        if (array[j] > array[j + 1]) {
+        if (array[j] > array[j + 1]) { // si el elemento es mayor que el siguiente
           SearchUtils.swap(array, j, j + 1);
         }
       }
