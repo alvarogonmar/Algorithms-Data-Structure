c765b08f32a4b1584721f4165f185b402286af74
alvarogonmar <alvarogonmar0@gmail.com>
2025-05-17 22:39:09 -0600

    Add comment to clarify the BubbleSort class definition

--- algoritmos-estructuras-datos-entrevistas-udemy-main/10-Sorting/src/main/java/com/danielblanco/algoritmosestructuras/sorting/_00_sorting/BubbleSort.java
+++ algoritmos-estructuras-datos-entrevistas-udemy-main/10-Sorting/src/main/java/com/danielblanco/algoritmosestructuras/sorting/_00_sorting/BubbleSort.java
@@ -1,6 +1,6 @@
 package com.danielblanco.algoritmosestructuras.sorting._00_sorting;
 
-public class BubbleSort {
+public class BubbleSort { // Bubble Sort
   static void sort(int[] array) {
     for (int i = 0; i < array.length - 1; i++) {
       for (int j = 0; j < array.length - i - 1; j++) {
