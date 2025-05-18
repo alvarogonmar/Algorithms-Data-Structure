7a3e3f43aaadd7d8c17e697bf565d259cee8363e
alvarogonmar <alvarogonmar0@gmail.com>
2025-05-17 22:39:34 -0600

    Add comment to clarify the inner loop iteration in the sort method of BubbleSort class

--- algoritmos-estructuras-datos-entrevistas-udemy-main/10-Sorting/src/main/java/com/danielblanco/algoritmosestructuras/sorting/_00_sorting/BubbleSort.java
+++ algoritmos-estructuras-datos-entrevistas-udemy-main/10-Sorting/src/main/java/com/danielblanco/algoritmosestructuras/sorting/_00_sorting/BubbleSort.java
@@ -3,7 +3,7 @@ package com.danielblanco.algoritmosestructuras.sorting._00_sorting;
 public class BubbleSort { // Bubble Sort
   static void sort(int[] array) { // método para ordenar el array
     for (int i = 0; i < array.length - 1; i++) { // recorre el array
-      for (int j = 0; j < array.length - i - 1; j++) {
+      for (int j = 0; j < array.length - i - 1; j++) { // recorre el array
         if (array[j] > array[j + 1]) {
           SearchUtils.swap(array, j, j + 1);
         }
