04f378a990ba6aa9f6e2e2ee49fccc2d4623ed87
alvarogonmar <alvarogonmar0@gmail.com>
2025-05-18 22:48:21 -0600

    Add comments to clarify the sorting process and merging logic in MergeSort class

--- algoritmos-estructuras-datos-entrevistas-udemy-main/10-Sorting/src/main/java/com/danielblanco/algoritmosestructuras/sorting/_00_sorting/MergeSort.java
+++ algoritmos-estructuras-datos-entrevistas-udemy-main/10-Sorting/src/main/java/com/danielblanco/algoritmosestructuras/sorting/_00_sorting/MergeSort.java
@@ -2,7 +2,7 @@ package com.danielblanco.algoritmosestructuras.sorting._00_sorting;
 
 public class MergeSort {
 
-  public static void sort(int[] array) {
+  public static void sort(int[] array) { // Método para ordenar el array
     int[] helper = new int[array.length];
     sort(array, helper, 0, array.length - 1);
   }
@@ -26,8 +26,11 @@ public class MergeSort {
     int helperRight = middle + 1;
     int current = low;
 
-    /* Itera el array auxiliar. Compara la mitad izquierda y la derecha, copiando el menor elemento
-     * de las dos mitades al array original. */
+    /*
+     * Itera el array auxiliar. Compara la mitad izquierda y la derecha, copiando el
+     * menor elemento
+     * de las dos mitades al array original.
+     */
     while (helperLeft <= middle && helperRight <= high) {
       if (helper[helperLeft] <= helper[helperRight]) {
         array[current] = helper[helperLeft];
@@ -39,12 +42,16 @@ public class MergeSort {
       current++;
     }
 
-    /* Copia el resto del lado izquierdo en el target array.
-     * Los elementos de la izquierda no se copian ya que ya se encuentran en el array. Por ejemplo
-     * 3 5 14 || 2 4 6. Por orden, copiamos 2, 3, 4, 5 y 6 al array, por lo que tenemos que copiar
+    /*
+     * Copia el resto del lado izquierdo en el target array.
+     * Los elementos de la izquierda no se copian ya que ya se encuentran en el
+     * array. Por ejemplo
+     * 3 5 14 || 2 4 6. Por orden, copiamos 2, 3, 4, 5 y 6 al array, por lo que
+     * tenemos que copiar
      * el resto de la izquierda (14)
      * Otro ejemplo: 2 4 6 || 3 5 14. Copiamos 2, 3, 4, 5 y 6 al array.
-     * El array helper es una copia del array, por lo que no tenemos que copiar esa parte,
+     * El array helper es una copia del array, por lo que no tenemos que copiar esa
+     * parte,
      * ya está en su posición.
      */
     int remaining = middle - helperLeft;
