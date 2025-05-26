e053688da5fe2e195450b2eec149d50a93feefc8
alvarogonmar <alvarogonmar0@gmail.com>
2025-05-25 23:22:09 -0600

    Add comment to clarify the storage of the element in the swap method

--- algoritmos-estructuras-datos-entrevistas-udemy-main/10-Sorting/src/main/java/com/danielblanco/algoritmosestructuras/sorting/_00_sorting/SearchUtils.java
+++ algoritmos-estructuras-datos-entrevistas-udemy-main/10-Sorting/src/main/java/com/danielblanco/algoritmosestructuras/sorting/_00_sorting/SearchUtils.java
@@ -2,7 +2,7 @@ package com.danielblanco.algoritmosestructuras.sorting._00_sorting;
 
 public class SearchUtils { // Clase de utilidades para búsquedas y ordenación
   static void swap(int[] array, int i, int j) { // Método para intercambiar dos elementos en un array
-    int tmp = array[i];
+    int tmp = array[i]; // Almacenamos el elemento en la posición i
     array[i] = array[j];
     array[j] = tmp;
   }
