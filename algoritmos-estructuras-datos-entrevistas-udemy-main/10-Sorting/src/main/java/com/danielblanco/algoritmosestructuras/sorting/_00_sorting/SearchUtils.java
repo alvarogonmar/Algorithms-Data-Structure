46ebe5b93b3a8f3b8a6fc589e19a3759a6fd15fc
alvarogonmar <alvarogonmar0@gmail.com>
2025-05-25 23:22:18 -0600

    Add comment to clarify the assignment operation in the swap method

--- algoritmos-estructuras-datos-entrevistas-udemy-main/10-Sorting/src/main/java/com/danielblanco/algoritmosestructuras/sorting/_00_sorting/SearchUtils.java
+++ algoritmos-estructuras-datos-entrevistas-udemy-main/10-Sorting/src/main/java/com/danielblanco/algoritmosestructuras/sorting/_00_sorting/SearchUtils.java
@@ -3,7 +3,7 @@ package com.danielblanco.algoritmosestructuras.sorting._00_sorting;
 public class SearchUtils { // Clase de utilidades para búsquedas y ordenación
   static void swap(int[] array, int i, int j) { // Método para intercambiar dos elementos en un array
     int tmp = array[i]; // Almacenamos el elemento en la posición i
-    array[i] = array[j];
+    array[i] = array[j]; // Asignamos el elemento en la posición j a la posición i
     array[j] = tmp;
   }
 }
