2e9e3ee861c0bb450aa05e7f136b65b82cdf23ab
alvarogonmar <alvarogonmar0@gmail.com>
2025-05-25 23:21:52 -0600

    Add comment to clarify the purpose of the SearchUtils class

--- algoritmos-estructuras-datos-entrevistas-udemy-main/10-Sorting/src/main/java/com/danielblanco/algoritmosestructuras/sorting/_00_sorting/SearchUtils.java
+++ algoritmos-estructuras-datos-entrevistas-udemy-main/10-Sorting/src/main/java/com/danielblanco/algoritmosestructuras/sorting/_00_sorting/SearchUtils.java
@@ -1,6 +1,6 @@
 package com.danielblanco.algoritmosestructuras.sorting._00_sorting;
 
-public class SearchUtils {
+public class SearchUtils { // Clase de utilidades para búsquedas y ordenación
   static void swap(int[] array, int i, int j) {
     int tmp = array[i];
     array[i] = array[j];
