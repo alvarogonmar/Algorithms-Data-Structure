8dbf20c2ab9385229ff7816f8451392ddc50ed57
alvarogonmar <alvarogonmar0@gmail.com>
2025-05-27 21:59:17 -0600

    Add comment to clarify the iteration process in findMinimumElementIndex method

--- algoritmos-estructuras-datos-entrevistas-udemy-main/10-Sorting/src/main/java/com/danielblanco/algoritmosestructuras/sorting/_00_sorting/SelectionSort.java
+++ algoritmos-estructuras-datos-entrevistas-udemy-main/10-Sorting/src/main/java/com/danielblanco/algoritmosestructuras/sorting/_00_sorting/SelectionSort.java
@@ -17,7 +17,7 @@ public class SelectionSort { // Algoritmo de ordenación por selección
                                                                        // mínimo en el array a partir de la posición
                                                                        // start
     int minIndex = start; // Inicializamos el índice mínimo con el valor de start
-    for (int i = start + 1; i < array.length; i++) {
+    for (int i = start + 1; i < array.length; i++) { // Recorremos el array desde start + 1 hasta el final
       if (array[i] < array[minIndex]) // Si el elemento actual es menor que el elemento en minIndex
         minIndex = i;
     }
