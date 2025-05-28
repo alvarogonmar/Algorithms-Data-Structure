e6d4e2040e6a258abeef13f24f02554c59df7c37
alvarogonmar <alvarogonmar0@gmail.com>
2025-05-27 21:57:53 -0600

    Refactor comments for clarity and readability in SelectionSort class

--- algoritmos-estructuras-datos-entrevistas-udemy-main/10-Sorting/src/main/java/com/danielblanco/algoritmosestructuras/sorting/_00_sorting/SelectionSort.java
+++ algoritmos-estructuras-datos-entrevistas-udemy-main/10-Sorting/src/main/java/com/danielblanco/algoritmosestructuras/sorting/_00_sorting/SelectionSort.java
@@ -1,10 +1,11 @@
 package com.danielblanco.algoritmosestructuras.sorting._00_sorting;
 
-public class SelectionSort {
+public class SelectionSort { // Algoritmo de ordenación por selección
   static void sort(int[] array) {
 
     for (int i = 0; i < array.length - 1; i++) {
-      // En cada iteración seleccionamos el mínimo y lo movemos al inicio de la parte desordenada
+      // En cada iteración seleccionamos el mínimo y lo movemos al inicio de la parte
+      // desordenada
       // del array.
       int minIndex = findMinimumElementIndex(array, i);
       SearchUtils.swap(array, i, minIndex);
@@ -14,7 +15,8 @@ public class SelectionSort {
   private static int findMinimumElementIndex(int[] array, int start) {
     int minIndex = start;
     for (int i = start + 1; i < array.length; i++) {
-      if (array[i] < array[minIndex]) minIndex = i;
+      if (array[i] < array[minIndex])
+        minIndex = i;
     }
 
     return minIndex;
