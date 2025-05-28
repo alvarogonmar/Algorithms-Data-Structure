5aef7e5fa4ab00ae068178852f6429cfae4a907c
alvarogonmar <alvarogonmar0@gmail.com>
2025-05-27 21:57:41 -0600

    Add comment to clarify the assignment of the temporary element in the swap method

--- algoritmos-estructuras-datos-entrevistas-udemy-main/10-Sorting/src/main/java/com/danielblanco/algoritmosestructuras/sorting/_00_sorting/SearchUtils.java
+++ algoritmos-estructuras-datos-entrevistas-udemy-main/10-Sorting/src/main/java/com/danielblanco/algoritmosestructuras/sorting/_00_sorting/SearchUtils.java
@@ -4,6 +4,6 @@ public class SearchUtils { // Clase de utilidades para búsquedas y ordenación
   static void swap(int[] array, int i, int j) { // Método para intercambiar dos elementos en un array
     int tmp = array[i]; // Almacenamos el elemento en la posición i
     array[i] = array[j]; // Asignamos el elemento en la posición j a la posición i
-    array[j] = tmp;
+    array[j] = tmp; // Asignamos el elemento temporal a la posición j
   }
 }
