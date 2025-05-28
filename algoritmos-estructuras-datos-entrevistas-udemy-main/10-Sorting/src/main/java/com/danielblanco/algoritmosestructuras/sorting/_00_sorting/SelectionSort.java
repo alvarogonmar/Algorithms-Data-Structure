6e42c09e9a75298405c21b5a142d4d50ae681d57
alvarogonmar <alvarogonmar0@gmail.com>
2025-05-27 21:59:10 -0600

    Add comment to clarify the comparison operation in findMinimumElementIndex method

--- algoritmos-estructuras-datos-entrevistas-udemy-main/10-Sorting/src/main/java/com/danielblanco/algoritmosestructuras/sorting/_00_sorting/SelectionSort.java
+++ algoritmos-estructuras-datos-entrevistas-udemy-main/10-Sorting/src/main/java/com/danielblanco/algoritmosestructuras/sorting/_00_sorting/SelectionSort.java
@@ -18,7 +18,7 @@ public class SelectionSort { // Algoritmo de ordenación por selección
                                                                        // start
     int minIndex = start; // Inicializamos el índice mínimo con el valor de start
     for (int i = start + 1; i < array.length; i++) {
-      if (array[i] < array[minIndex])
+      if (array[i] < array[minIndex]) // Si el elemento actual es menor que el elemento en minIndex
         minIndex = i;
     }
 
