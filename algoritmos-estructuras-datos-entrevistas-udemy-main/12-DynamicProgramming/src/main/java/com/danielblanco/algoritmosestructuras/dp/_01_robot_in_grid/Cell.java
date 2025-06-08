003fd31b8cf7058514aa90317d1817253175a606
alvarogonmar <alvarogonmar0@gmail.com>
2025-06-07 21:39:55 -0600

    Add comments to clarify hashCode method implementation in Cell class

--- algoritmos-estructuras-datos-entrevistas-udemy-main/12-DynamicProgramming/src/main/java/com/danielblanco/algoritmosestructuras/dp/_01_robot_in_grid/Cell.java
+++ algoritmos-estructuras-datos-entrevistas-udemy-main/12-DynamicProgramming/src/main/java/com/danielblanco/algoritmosestructuras/dp/_01_robot_in_grid/Cell.java
@@ -16,7 +16,7 @@ public class Cell { // Clase que representa una celda en una cuadrícula, utiliz
     final int prime = 31; // Número primo utilizado para la generación del código hash
     int result = 1; // Inicializa el resultado del código hash en 1
     result = prime * result + row; // Multiplica el resultado por el número primo y suma la fila
-    result = prime * result + col;
+    result = prime * result + col; // Multiplica el resultado por el número primo y suma la columna
     return result;
   }
 
