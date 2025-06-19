9a931e3972e33a835fe6273c2205ba33aef33e38
alvarogonmar <alvarogonmar0@gmail.com>
2025-06-19 16:38:26 -0600

    Add comment to clarify the purpose of the recursive method in GenerateParenthesis class

--- algoritmos-estructuras-datos-entrevistas-udemy-main/12-DynamicProgramming/src/main/java/com/danielblanco/algoritmosestructuras/dp/_03_generate_parenthesis/GenerateParenthesis.java
+++ algoritmos-estructuras-datos-entrevistas-udemy-main/12-DynamicProgramming/src/main/java/com/danielblanco/algoritmosestructuras/dp/_03_generate_parenthesis/GenerateParenthesis.java
@@ -33,7 +33,7 @@ public class GenerateParenthesis { // Clase que representa el problema de genera
     return result; // Retorna la lista de combinaciones válidas de paréntesis
   }
 
-  private void generateParenthesis(
+  private void generateParenthesis( // Método recursivo que genera combinaciones válidas de paréntesis
       int openLeft, int closeLeft, StringBuilder current, List<String> result) {
     if (openLeft == 0 && closeLeft == 0) {
       result.add(current.toString());
