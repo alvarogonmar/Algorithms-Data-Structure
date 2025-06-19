a3901828bf826a52b03ad16cfc1f0c334cb25747
alvarogonmar <alvarogonmar0@gmail.com>
2025-06-19 16:39:04 -0600

    Add comment to clarify the return statement in the recursive generateParenthesis method

--- algoritmos-estructuras-datos-entrevistas-udemy-main/12-DynamicProgramming/src/main/java/com/danielblanco/algoritmosestructuras/dp/_03_generate_parenthesis/GenerateParenthesis.java
+++ algoritmos-estructuras-datos-entrevistas-udemy-main/12-DynamicProgramming/src/main/java/com/danielblanco/algoritmosestructuras/dp/_03_generate_parenthesis/GenerateParenthesis.java
@@ -37,7 +37,7 @@ public class GenerateParenthesis { // Clase que representa el problema de genera
       int openLeft, int closeLeft, StringBuilder current, List<String> result) { // Parámetros:
     if (openLeft == 0 && closeLeft == 0) { // Verifica si no quedan paréntesis por agregar
       result.add(current.toString()); // Agrega la combinación actual a la lista de resultados
-      return;
+      return; // Retorna si no quedan paréntesis por agregar
     }
 
     if (openLeft > 0) {
