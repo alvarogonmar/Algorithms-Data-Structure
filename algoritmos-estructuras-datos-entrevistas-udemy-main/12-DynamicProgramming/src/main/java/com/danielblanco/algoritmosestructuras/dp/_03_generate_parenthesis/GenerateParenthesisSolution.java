51260a93f2df9828cde242869bf82dc9df954050
alvarogonmar <alvarogonmar0@gmail.com>
2025-06-20 16:32:57 -0600

    Add comment to clarify the parameters of the recursive generateParenthesis method

--- algoritmos-estructuras-datos-entrevistas-udemy-main/12-DynamicProgramming/src/main/java/com/danielblanco/algoritmosestructuras/dp/_03_generate_parenthesis/GenerateParenthesisSolution.java
+++ algoritmos-estructuras-datos-entrevistas-udemy-main/12-DynamicProgramming/src/main/java/com/danielblanco/algoritmosestructuras/dp/_03_generate_parenthesis/GenerateParenthesisSolution.java
@@ -34,7 +34,7 @@ public class GenerateParenthesisSolution { // Clase que representa el problema d
   }
 
   private void generateParenthesis( // Método recursivo que genera combinaciones válidas de paréntesis
-      int openLeft, int closeLeft, StringBuilder current, List<String> result) {
+      int openLeft, int closeLeft, StringBuilder current, List<String> result) { // Parámetros:
     if (openLeft == 0 && closeLeft == 0) {
       result.add(current.toString());
       return;
