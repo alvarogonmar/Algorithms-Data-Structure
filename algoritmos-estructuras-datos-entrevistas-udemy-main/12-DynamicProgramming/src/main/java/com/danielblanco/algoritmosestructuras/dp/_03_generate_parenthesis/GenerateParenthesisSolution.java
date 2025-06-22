c3230ed74e90df7c214ffccef0cc10f84940f43f
alvarogonmar <alvarogonmar0@gmail.com>
2025-06-21 19:57:00 -0600

    Add comment to clarify the removal of the last character in the StringBuilder after adding a closing parenthesis

--- algoritmos-estructuras-datos-entrevistas-udemy-main/12-DynamicProgramming/src/main/java/com/danielblanco/algoritmosestructuras/dp/_03_generate_parenthesis/GenerateParenthesisSolution.java
+++ algoritmos-estructuras-datos-entrevistas-udemy-main/12-DynamicProgramming/src/main/java/com/danielblanco/algoritmosestructuras/dp/_03_generate_parenthesis/GenerateParenthesisSolution.java
@@ -48,7 +48,7 @@ public class GenerateParenthesisSolution { // Clase que representa el problema d
 
     if (closeLeft > openLeft) { // Verifica si quedan paréntesis de cierre por agregar
       generateParenthesis(openLeft, closeLeft - 1, current.append(')'), result); // Llama al método recursivo para
-      current.setLength(current.length() - 1);
+      current.setLength(current.length() - 1); // agregar un paréntesis de cierre
     }
   }
 }
